e393e63c40cf720dbbd0481cdb86c96bdd6a5d75
Khalil Peguero <khalilpeguero@hotmail.com>
2022-09-01 23:01:41 -0400

    Added first files
    I got the idea a couple of hours ago so I just put a few lines of code and a text file with the basic idea for the project. I have exactly 10 days to finish this and the other gifts I'm preparing for Maria so... let's see.

---
+++ Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("\n\n\n\n\n\n\nWelcome Maria");
+
+        System.out.println("Here are your options:");
+        System.out.println("\n0. Introduction");
+        System.out.println("1. September");
+        System.out.println("2. October");
+        System.out.println("3. November");
+        System.out.println("4. December");
+        System.out.println("5. January");
+        System.out.println("6. February");
+        System.out.println("7. March");
+        System.out.println("8. April");
+        System.out.println("9. May");
+        System.out.println("10. June");
+        System.out.println("11. July");
+        System.out.println("12. August");
+
+
+
+
+
+
+
+
+        input.close();
+    }
+}
